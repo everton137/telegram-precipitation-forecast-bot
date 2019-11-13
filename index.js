031f0c22018eb8726db121cbdf6cc76aa9033d45
Everton Zanella <eza@protonmail.ch>
2019-11-13 12:06:30 +0100

    Returning city temperature and using axios instead of fetch

--- index.js
+++ index.js
@@ -1,45 +1,40 @@
-const TelegramBot = require('node-telegram-bot-api');
-const fetch = require('node-fetch');
-const axios = require('axios');
-require('dotenv').config();
+const TelegramBot = require("node-telegram-bot-api");
+const axios = require("axios");
+require("dotenv").config();
 
 const token = process.env.TELEGRAM_API_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
+let telegramUrl = `https://api.telegram.org/bot${process.env.TELEGRAM_API_TOKEN}/`
+
 async function getForecast(city) {
-    // Get geolocation from Nominatim OSM
-    let nominatimUrl = `https://nominatim.openstreetmap.org/search.php?q=${city}&format=json`;
-    let geolocation = {};
-    let nominatimResp = await fetch(nominatimUrl);
-    let nominatimData = await nominatimResp.json();
-    geolocation.lat = nominatimData[0].lat;
-    geolocation.lon = nominatimData[0].lon;
-    // Get weather from Darksky
-    let darkskyUrl = `https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${geolocation.lat},${geolocation.lon}?units=si`;
-    let darkskyResp = await fetch(darkskyUrl);
-    let darkskyData = await darkskyResp.json();
-    return darkskyData.currently;
+  // Get geolocation from Nominatim OSM
+  let nominatimUrl = `https://nominatim.openstreetmap.org/search.php?q=${city}&format=json`;
+  let geolocation = {};
+  let nominatimResp = await axios.get(nominatimUrl);
+  let nominatimData = await nominatimResp.data;
+  geolocation.lat = nominatimData[0].lat;
+  geolocation.lon = nominatimData[0].lon;
+  // Get weather from Darksky
+  let darkskyUrl = `https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${geolocation.lat},${geolocation.lon}?units=si`;
+  let darkskyResp = await axios.get(darkskyUrl);
+  let darkskyData = await darkskyResp.data;
+  return darkskyData.currently;
 }
 
-// Printing an example to test 
-(async () => {
-    console.log(await getForecast('Berlin'))
-})();
+// Matches "/city [city name]"
+bot.onText(/\/city (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
 
-// Test using axios
-function getGeolocation(city) {
-    let nominatimUrl = `https://nominatim.openstreetmap.org/search.php?q=${city}&format=json`;
-    let geolocation = {};
-    return axios.get(nominatimUrl)
-        .then(response => {
-            let data = response.data;
-            geolocation.lat = data[0].lat;
-            geolocation.lon = data[0].lon;
-            return geolocation;
-        });
-}
+  const city = match[1]; // the captured "city name"
+  getForecast(city).then(response => {
+    let text = `Now it's ${response.temperature} °C in ${city}`;
+    let sendMessageParams = { chat_id: msg.chat.id, text: text, pase_mode: 'Markdown'};
+    axios.post(telegramUrl + 'sendMessage', sendMessageParams);
+  });
 
-bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, 'Please, type /help for more information.')
 });
 
+// bot.on('polling_error', err => console.log(err));
