2456395a98b3e88049cfe22b9dd5ff91678cbba5
Everton Zanella <eza@protonmail.ch>
2019-11-13 15:49:03 +0100

    Adding moment to format the dates

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const TelegramBot = require("node-telegram-bot-api");
 const axios = require("axios");
+const moment = require("moment");
 require("dotenv").config();
 
 const token = process.env.TELEGRAM_API_TOKEN;
@@ -27,9 +28,9 @@ bot.onText(/\/help/, msg => {
   // 'msg' is the received Message from Telegram
   text = `
 Type:
-  /temp [city name] - to get the current temperature
   /rain [city name] - to get the rain forecast for the next 7 days
-`;
+  /temp [city name] - to get the current temperature
+  `;
 
   bot.sendMessage(msg.chat.id, text);
 
@@ -44,10 +45,10 @@ bot.onText(/\/rain (.+)/, (msg, match) => {
   const city = match[1]; // the captured "city name"
   getForecast(city).then(resp => {
     let text = `
-The chances of rain in ${city} the 7 five days are:
- (today) ${Math.round(resp.currently.precipProbability * 100)}%\n`;
+The chance of rain in ${city} in the next 7 five days is:
+ ${moment().format('ddd')}: ${Math.round(resp.currently.precipProbability * 100)}% (today)\n`;
     for (let i=1; i < 7; i++) {
-      text += ` (${i}) ${Math.round(resp.daily.data[i].precipProbability * 100)}% \n`;
+      text += ` ${moment().add(i, 'days').format('ddd')}: ${Math.round(resp.daily.data[i].precipProbability * 100)}% \n`;
     }
     let sendMessageParams = { chat_id: msg.chat.id, text: text, parse_mode: 'Markdown'};
     axios.post(telegramUrl + 'sendMessage', sendMessageParams);
