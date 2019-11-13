fb71d1b90b1eaf9489447cd5297ceb9017a8cd4f
Everton Zanella <eza@protonmail.ch>
2019-11-13 14:59:55 +0100

    Rain forecast for the next 7 days (format to be improved)

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ async function getForecast(city) {
   let darkskyUrl = `https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${geolocation.lat},${geolocation.lon}?units=si`;
   let darkskyResp = await axios.get(darkskyUrl);
   let darkskyData = await darkskyResp.data;
-  return darkskyData.currently;
+  return darkskyData;
 }
 
 // Bot /help
@@ -27,28 +27,45 @@ bot.onText(/\/help/, msg => {
   // 'msg' is the received Message from Telegram
   text = `
 Type:
-  /city [city name]
-to get the current temperature.`;
+  /temp [city name] - to get the current temperature
+  /rain [city name] - to get the rain forecast for the next 7 days
+`;
 
   bot.sendMessage(msg.chat.id, text);
 
 });
 
+// Matches "/rain [city name]"
+bot.onText(/\/rain (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
 
+  const city = match[1]; // the captured "city name"
+  getForecast(city).then(resp => {
+    let text = `
+The chances of rain in ${city} the 7 five days are:
+ (today) ${Math.round(resp.currently.precipProbability * 100)}%\n`;
+    for (let i=1; i < 7; i++) {
+      text += ` (${i}) ${Math.round(resp.daily.data[i].precipProbability * 100)}% \n`;
+    }
+    let sendMessageParams = { chat_id: msg.chat.id, text: text, parse_mode: 'Markdown'};
+    axios.post(telegramUrl + 'sendMessage', sendMessageParams);
+  });
+});
 
-// Matches "/city [city name]"
-bot.onText(/\/city (.+)/, (msg, match) => {
+// Matches "/temp [city name]"
+bot.onText(/\/temp (.+)/, (msg, match) => {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
 
   const city = match[1]; // the captured "city name"
   getForecast(city).then(response => {
-    let text = `Now it's *${response.temperature} °C* in ${city}`;
+    let text = `Now it's *${response.currently.temperature} °C* in *${city}*`;
     let sendMessageParams = { chat_id: msg.chat.id, text: text, parse_mode: 'Markdown'};
     axios.post(telegramUrl + 'sendMessage', sendMessageParams);
   });
-
 });
 
 // bot.on('polling_error', err => console.log(err));
