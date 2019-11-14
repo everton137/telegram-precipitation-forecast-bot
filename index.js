a18c6b5135b817e23d00d7b0b778baf8a6f53a0f
Everton Zanella Alvarenga <eza@pm.me>
2019-11-14 12:17:59 +0100

    Wind direction in cardinal

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ async function getForecast(city) {
   return darkskyData;
 }
 
+// Function to remove accents from a string
 // Source: https://gist.github.com/alisterlf/3490957
 function removeAccents(str) {
   let accents = 'ÀÁÂÃÄÅàáâãäåßÒÓÔÕÕÖØòóôõöøÈÉÊËèéêëðÇçÐÌÍÎÏìíîïÙÚÛÜùúûüÑñŠšŸÿýŽž';
@@ -35,6 +36,45 @@ function removeAccents(str) {
     }
   })
   return str.join('');
+}
+
+// Convert degree to cardinal. Directions source: http://snowfence.umn.edu/Components/winddirectionanddegreeswithouttable3.htm
+function degToCard(deg) {
+  let direction;
+  if (deg > 349 && deg <= 11) {
+    direction = 'N';
+  } else if (deg > 11 && deg <= 34) {
+    direction = 'NNE';
+  } else if (deg > 34 && deg <= 56) {
+    direction = 'NE';
+  } else if (deg > 56 && deg <= 79) {
+    direction = 'ENE';
+  } else if (deg > 79 && deg <= 101) {
+    direction = 'E';
+  } else if (deg > 101 && deg <= 124) {
+    direction = 'ESE';
+  } else if (deg > 124 && deg <= 146) {
+    direction = 'SE';
+  } else if (deg > 146 && deg <= 169) {
+    direction = 'SSE';
+  } else if (deg > 169 && deg <= 191) {
+    direction = 'S';
+  } else if (deg > 191 && deg <= 214) {
+    direction = 'SSW';
+  } else if (deg > 214 && deg <= 236) {
+    direction = 'SW';
+  } else if (deg > 236 && deg <= 259) {
+    direction = 'WSW';
+  } else if (deg > 259 && deg <= 281) {
+    direction = 'W';
+  } else if (deg > 281 && deg <= 304) {
+    direction = 'WNW';
+  } else if (deg > 304 && deg <= 326) {
+    direction = 'NW';
+  } else if (deg > 326 && deg <= 349) {
+    direction = 'NNW';
+  } 
+  return direction;
 }
 
 // Bot /help
@@ -78,13 +118,14 @@ bot.onText(/\/temp (.+)/, (msg, match) => {
   // 'match' is the result of executing the regexp above on the text content
   // of the message
 
+
   const city = match[1]; // the captured "city name"
   const cityNoAccents = removeAccents(match[1]);
 
   getForecast(cityNoAccents).then(response => {
     let text = `
 Now it's *${response.currently.temperature}* °C (feels like *${response.currently.apparentTemperature}* °C) in *${city}*
-Wind: speed *${(response.currently.windSpeed * 3.6).toFixed(1)}* (km/h), gust *${(response.currently.windGust * 3.6).toFixed(1)}* (km/h), direction *${response.currently.windBearing}*°
+Wind: speed *${(response.currently.windSpeed * 3.6).toFixed(1)}* (km/h), gust *${(response.currently.windGust * 3.6).toFixed(1)}* (km/h), direction *${degToCard(response.currently.windBearing)}*
 Humidity: *${Math.round(response.currently.humidity * 100)}%*, UV Index: *${response.currently.uvIndex}*
 `;
 //*Sunrise/Sunset*: ${moment(response.daily.data[0].sunriseTime).format('HH:mm')} / ${moment(response.daily.data[0].sunsetTime).format('HH:mm')}
