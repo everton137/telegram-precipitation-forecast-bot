f8e245ad855eadffca4b23c0175acc8678e9c195
Everton Zanella Alvarenga <eza@protonmail.ch>
2019-11-15 11:17:01 +0100

    Creating auxiliary module

---
+++ aux.js
@@ -0,0 +1,57 @@
+// Auxiliary Functions
+
+// Remove accents from a string
+// Source: https://gist.github.com/alisterlf/3490957
+exports.removeAccents = function(str) {
+    let accents = 'ÀÁÂÃÄÅàáâãäåßÒÓÔÕÕÖØòóôõöøÈÉÊËèéêëðÇçÐÌÍÎÏìíîïÙÚÛÜùúûüÑñŠšŸÿýŽž';
+    let accentsOut = "AAAAAAaaaaaaBOOOOOOOooooooEEEEeeeeeCcDIIIIiiiiUUUUuuuuNnSsYyyZz";
+    str = str.split('');
+    str.forEach((letter, index) => {
+        let i = accents.indexOf(letter);
+        if (i != -1) {
+            str[index] = accentsOut[i];
+        }
+    })
+    return str.join('');
+}
+
+// Convert degree to cardinal
+// Directions source: http://snowfence.umn.edu/Components/winddirectionanddegreeswithouttable3.htm
+exports.degToCard = function(deg) {
+    let direction = '';
+    if (deg > 11 && deg <= 34) {
+        direction = 'NNE';
+    } else if (deg > 34 && deg <= 56) {
+        direction = 'NE';
+    } else if (deg > 56 && deg <= 79) {
+        direction = 'ENE';
+    } else if (deg > 79 && deg <= 101) {
+        direction = 'E';
+    } else if (deg > 101 && deg <= 124) {
+        direction = 'ESE';
+    } else if (deg > 124 && deg <= 146) {
+        direction = 'SE';
+    } else if (deg > 146 && deg <= 169) {
+        direction = 'SSE';
+    } else if (deg > 169 && deg <= 191) {
+        direction = 'S';
+    } else if (deg > 191 && deg <= 214) {
+        direction = 'SSW';
+    } else if (deg > 214 && deg <= 236) {
+        direction = 'SW';
+    } else if (deg > 236 && deg <= 259) {
+        direction = 'WSW';
+    } else if (deg > 259 && deg <= 281) {
+        direction = 'W';
+    } else if (deg > 281 && deg <= 304) {
+        direction = 'WNW';
+    } else if (deg > 304 && deg <= 326) {
+        direction = 'NW';
+    } else if (deg > 326 && deg <= 349) {
+        direction = 'NNW';
+    } else {
+        direction = 'N';
+    }
+    console.log(deg);
+    return direction;
+}
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const TelegramBot = require("node-telegram-bot-api");
 const axios = require("axios");
 const moment = require("moment");
 require("dotenv").config();
+const aux = require('./aux');
 
 const token = process.env.TELEGRAM_API_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
@@ -21,60 +22,6 @@ async function getForecast(city) {
   let darkskyResp = await axios.get(darkskyUrl);
   let darkskyData = await darkskyResp.data;
   return darkskyData;
-}
-
-// Function to remove accents from a string
-// Source: https://gist.github.com/alisterlf/3490957
-function removeAccents(str) {
-  let accents = 'ÀÁÂÃÄÅàáâãäåßÒÓÔÕÕÖØòóôõöøÈÉÊËèéêëðÇçÐÌÍÎÏìíîïÙÚÛÜùúûüÑñŠšŸÿýŽž';
-  let accentsOut = "AAAAAAaaaaaaBOOOOOOOooooooEEEEeeeeeCcDIIIIiiiiUUUUuuuuNnSsYyyZz";
-  str = str.split('');
-  str.forEach((letter, index) => {
-    let i = accents.indexOf(letter);
-    if (i != -1) {
-      str[index] = accentsOut[i];
-    }
-  })
-  return str.join('');
-}
-
-// Convert degree to cardinal. Directions source: http://snowfence.umn.edu/Components/winddirectionanddegreeswithouttable3.htm
-function degToCard(deg) {
-  let direction;
-  if (deg > 349 && deg <= 11) {
-    direction = 'N';
-  } else if (deg > 11 && deg <= 34) {
-    direction = 'NNE';
-  } else if (deg > 34 && deg <= 56) {
-    direction = 'NE';
-  } else if (deg > 56 && deg <= 79) {
-    direction = 'ENE';
-  } else if (deg > 79 && deg <= 101) {
-    direction = 'E';
-  } else if (deg > 101 && deg <= 124) {
-    direction = 'ESE';
-  } else if (deg > 124 && deg <= 146) {
-    direction = 'SE';
-  } else if (deg > 146 && deg <= 169) {
-    direction = 'SSE';
-  } else if (deg > 169 && deg <= 191) {
-    direction = 'S';
-  } else if (deg > 191 && deg <= 214) {
-    direction = 'SSW';
-  } else if (deg > 214 && deg <= 236) {
-    direction = 'SW';
-  } else if (deg > 236 && deg <= 259) {
-    direction = 'WSW';
-  } else if (deg > 259 && deg <= 281) {
-    direction = 'W';
-  } else if (deg > 281 && deg <= 304) {
-    direction = 'WNW';
-  } else if (deg > 304 && deg <= 326) {
-    direction = 'NW';
-  } else if (deg > 326 && deg <= 349) {
-    direction = 'NNW';
-  } 
-  return direction;
 }
 
 // Bot /help
@@ -98,7 +45,7 @@ bot.onText(/\/rain (.+)/, (msg, match) => {
   // of the message
 
   const city = match[1];
-  const cityNoAccents = removeAccents(match[1]); // the captured "city name"
+  const cityNoAccents = aux.removeAccents(match[1]); // the captured "city name"
 
   getForecast(cityNoAccents).then(resp => {
     let text = `
@@ -120,12 +67,12 @@ bot.onText(/\/temp (.+)/, (msg, match) => {
 
 
   const city = match[1]; // the captured "city name"
-  const cityNoAccents = removeAccents(match[1]);
+  const cityNoAccents = aux.removeAccents(match[1]);
 
   getForecast(cityNoAccents).then(response => {
     let text = `
 Now it's *${response.currently.temperature}* °C (feels like *${response.currently.apparentTemperature}* °C) in *${city}*
-Wind: speed *${(response.currently.windSpeed * 3.6).toFixed(1)}* (km/h), gust *${(response.currently.windGust * 3.6).toFixed(1)}* (km/h), direction *${degToCard(response.currently.windBearing)}*
+Wind: speed *${(response.currently.windSpeed * 3.6).toFixed(1)}* (km/h), gust *${(response.currently.windGust * 3.6).toFixed(1)}* (km/h), direction *${aux.degToCard(response.currently.windBearing)}*
 Sunrise/Sunset: *${moment.unix(response.daily.data[0].sunriseTime).format('HH:mm')}* / *${moment.unix(response.daily.data[0].sunsetTime).format('HH:mm')}*
 Humidity: *${Math.round(response.currently.humidity * 100)}%*, UV Index: *${response.currently.uvIndex}*
 `;
@@ -141,7 +88,7 @@ bot.onText(/\/maxmin (.+)/, (msg, match) => {
   // of the message
 
   const city = match[1]; // the captured "city name"
-  const cityNoAccents = removeAccents(match[1]);
+  const cityNoAccents = aux.removeAccents(match[1]);
 
   getForecast(cityNoAccents).then(resp => {
     let text = `
